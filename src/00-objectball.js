a5054e8c66fdd239d2f2998d2c79012cdc37f0cf
Bruno Trova <bltrova@gmail.com>
2025-01-29 09:28:28 -0800

    obejcts-keys

---
+++ src/00-objectball.js
@@ -0,0 +1,27 @@
+function gameObeject() {
+    home: {
+        teamName: "Brooklyn Nets"
+        colors: [Black, White],
+        players: {"Alan Anderson" : {
+            Number: 0,
+            Shoe: 16,
+            Points: 22
+            Rebounds: 12,
+            Assists: 12
+            Steals: 3,
+            Blocks: 1
+            Slam Dunks: 1
+
+
+        }
+
+        }
+    ]
+    away: {
+        teamName: ""
+        colors: [...],
+        players: {
+
+        }
+    }
+}
